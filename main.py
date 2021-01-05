6cb0dd1a7a9b9231fa6b304607151316d4b6fa02
Colleen <cmlohr@outlook.com>
2021-01-04 20:27:53 -0600

    checkpoint update, added: rating edit, sqlalchemy, database

--- main.py
+++ main.py
@@ -1,39 +1,70 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 Bootstrap(app)
 
-archive = []
+# create sqlalchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-archive.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db = sqlite3.connect("archive.db")
-cursor = db.cursor()
-# db creation
-# cursor.execute("CREATE TABLE archive (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
-#                "NOT NULL, url varchar(250) NOT NULL UNIQUE, rating FLOAT NOT NULL)")
-cursor.execute("INSERT INTO archive VALUES(1, 'Beowulf', 'Unknown', "
-               "'https://archive.org/stream/cu31924013339373/cu31924013339373_djvu.txt', '9.5')")
-db.commit()
+
+# sqlalchemy below
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    url = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
+
 
 @app.route('/')
 def home():
+    archive = db.session.query(Story).all()
     return render_template("index.html", archive=archive)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_story = {
-            "title": request.form["title"],
-            "url": request.form["url"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        archive.append(new_story)
+        new_story = Story(
+            title=request.form["title"],
+            url=request.form["url"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_story)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        story_id = request.form["id"]
+        story_update = Story.query.get(story_id)
+        story_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    story_id = request.args.get('id')
+    story_selected = Story.query.get(story_id)
+    return render_template("rating.html", story=story_selected)
+
+
+@app.route("/delete")
+def delete():
+    story_id = request.args.get('id')
+    story_to_delete = Story.query.get(story_id)
+    db.session.delete(story_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
