dad17eae2479f8c5954a08ba5fb01b4ea5a46bb7
Colleen <cmlohr@outlook.com>
2021-01-06 23:17:49 -0600

    added new routes for add edit and delete

--- archive-listicle/main.py
+++ archive-listicle/main.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
-import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345678'
@@ -21,6 +20,7 @@ class TopTen(db.Model):
     url = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     author = db.Column(db.String(250), nullable=False)
+    review = db.Column(db.String(250), nullable=True)
     rating = db.Column(db.Float, nullable=False)
     ranking = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(1000), nullable=False)
@@ -28,14 +28,38 @@ class TopTen(db.Model):
 
 
 db.create_all()
-# game = True
-# if game:
+
+
+class AddStory(FlaskForm):
+    title = StringField("Story Title", validators=[DataRequired()])
+    author = StringField("Story Author", validators=[DataRequired()])
+    year = StringField("Year Published", validators=[DataRequired()])
+    rating = StringField("Rating", validators=[DataRequired()])
+    url = StringField("Story URL", validators=[DataRequired()])
+    img_url = StringField("Story Image URL", validators=[DataRequired()])
+    review = StringField("Review", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    ranking = StringField("Rank", validators=[DataRequired()])
+    submit = SubmitField("Add Story")
+
+
+class RateStory(FlaskForm):
+    rating = StringField("Rating")
+    review = StringField("Review")
+    ranking = StringField("Ranking")
+    description = TextAreaField("Description")
+    submit = SubmitField("Done")
+
+
+# add_story = True
+# if add_story:
 #     test_story = TopTen(
 #         id=1,
 #         url='www.story.com',
 #         title='HatsCats',
 #         year=2021,
 #         author='Dude',
+#         review='Cool Beans!!',
 #         rating=5.3,
 #         ranking=1,
 #         description='yo words and stuff',
@@ -46,17 +70,53 @@ db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    listicle = TopTen.query.all()
+    return render_template("index.html", listicle=listicle)
 
 
-@app.route("/edit")
-def edit():
-    return render_template("edit.html")
+@app.route("/edit", methods=["GET", "POST"])
+def rate_story():
+    form = RateStory()
+    story_id = request.args.get("id")
+    story = TopTen.query.get(story_id)
+    if form.validate_on_submit():
+        story.rating = float(form.rating.data)
+        story.review = form.review.data
+        story.ranking = form.ranking.data
+        story.description = form.description.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", story=story, form=form)
 
 
-@app.route("/add")
+@app.route("/delete")
+def delete_story():
+    story_id = request.args.get("id")
+    story = TopTen.query.get(story_id)
+    db.session.delete(story)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template("add.html")
+    form = AddStory()
+    if request.method == "POST":
+        story = TopTen(
+            url=request.form["url"],
+            title=request.form["title"],
+            year=request.form["year"],
+            author=request.form["author"],
+            review=request.form["review"],
+            rating=request.form["rating"],
+            description=request.form["description"],
+            img_url=request.form["img_url"],
+            ranking=request.form["ranking"]
+        )
+        db.session.add(story)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html", form=form)
 
 
 @app.route("/select")
